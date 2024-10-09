b1d14f48f4d90f338b987354774f6a9dfcb61bd3
Wendy-Vid-H <yh842@georgetown.edu>
2024-10-09 18:02:22 -0400

    add first draft code

---
+++ draft2.R
@@ -0,0 +1,5 @@
+library(tidyverse)
+
+library(readxl)
+SDOH_2020_COUNTY <- read_excel("E:/Graduate-2024fall/intro_to_data_science/assignment04/sdoh-data/SDOH_2020_COUNTY_1_0.xlsx", 
+                               sheet = "Data")
